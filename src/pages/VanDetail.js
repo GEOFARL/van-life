215d9cb216bf7375735e97bef280b0c0b01d84c6
Maxim <toper.one111@gmail.com>
2023-06-19 16:43:52 +0300

    Implment detail page for a van

--- src/pages/VanDetail.js
+++ src/pages/VanDetail.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const VanDetail = () => {
-  return <h1>Van detail page goes here</h1>;
+  const params = useParams();
+  const [van, setVan] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`/api/vans/${params.id}`);
+        const data = await response.json();
+        setVan(data.vans);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [params.id]);
+
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt="van" />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 };
