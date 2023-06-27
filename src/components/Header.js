e01a20233011aa6f36a61004f772e2d7c6239dda
Maxim <toper.one111@gmail.com>
2023-06-27 16:11:10 +0300

    Add active styling for main navbar on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Header = () => {
+  const activeStyle = {
+    fontWeight: 'bold',
+    textDecoration: 'underline',
+    color: '#161616',
+  };
+
   return (
     <header>
       <Link className="site-logo" to="/">
         #VANLIFE
       </Link>
       <nav>
-        <Link to="/vans">Vans</Link>
-        <Link to="/about">About</Link>
-        <Link to="/host">Host</Link>
+        <NavLink
+          to="/vans"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/host"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Host
+        </NavLink>
       </nav>
     </header>
   );
