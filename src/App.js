07d87d38d4ae382484feb45801a837c518df42f8
Maxim <toper.one111@gmail.com>
2023-06-27 14:54:11 +0300

    Separate layout and header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Vans } from './pages/Vans';
 import { VanDetail } from './pages/VanDetail';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/vans">Vans</Link>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
