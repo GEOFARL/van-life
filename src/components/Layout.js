00d175d3c7a260a97c629715acff160840a48fb5
Maxim <toper.one111@gmail.com>
2023-06-27 15:56:27 +0300

    Add footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Header } from './Header';
 import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
 
 export const Layout = () => {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className="site-wrapper">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 };
