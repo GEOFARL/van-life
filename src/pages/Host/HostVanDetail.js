e0a7f4c930f08495e549ac91caada4982a27918a
Maxim <toper.one111@gmail.com>
2023-06-30 19:46:02 +0300

    Add navbar to the detail page

--- src/pages/Host/HostVanDetail.js
+++ src/pages/Host/HostVanDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link, Outlet } from 'react-router-dom';
+import { useParams, Link, Outlet, NavLink } from 'react-router-dom';
 
 export function HostVanDetail() {
   const { id } = useParams();
@@ -14,6 +14,13 @@ export function HostVanDetail() {
   if (!currentVan) {
     return <h1>Loading...</h1>;
   }
+
+  const activeStyles = {
+    fontWeight: 'bold',
+    textDecoration: 'underline',
+    color: '#161616',
+  };
+
   return (
     <section>
       <Link to=".." relative="path" className="back-button">
@@ -31,6 +38,27 @@ export function HostVanDetail() {
             <h4>${currentVan.price}/day</h4>
           </div>
         </div>
+        <nav className="host-van-detail-nav">
+          <NavLink
+            to="."
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+            end
+          >
+            Details
+          </NavLink>
+          <NavLink
+            to="pricing"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Pricing
+          </NavLink>
+          <NavLink
+            to="photos"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Photos
+          </NavLink>
+        </nav>
         <Outlet />
       </div>
     </section>
